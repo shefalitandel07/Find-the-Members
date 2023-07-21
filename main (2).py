0298544d994df6fcd454aa4b8f2a29295396773d
shefalitandel07 <134202324+shefalitandel07@users.noreply.github.com>
2023-07-21 14:59:50 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,54 @@
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ; C    (#) `-.o `"=.`_.--"_o.-; ;___|___________________     A
+____/______/______/___|o;._     R      `".o|o_.--"    ;o;____/______/______/____         A
+/______/______/______/_"=._o--._      T      ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Carat's Island!!!")
+print("Your mission is to find Members of Seventeen.") 
+
+choice1 = input('You\'re at a Hybes Building. \nWhich floor do you want to go? \nType floor no: 6,4,17,30,15,7,8,22,16,10,18,11,2 \n').lower()
+
+if choice1 == "2":
+  print("Carat!! So Lucky You met all the members")
+elif choice1 == "6":
+  print("Carat!! You found Mingyu!")
+elif choice1 == "4":
+  print("Carat!! You found Jeonghan!")
+elif choice1 == "17":
+  print("Carat!! You found Wonwoo!")
+elif choice1 == "30":
+  print("Carat!! You found Joshua!")
+elif choice1 == "15":
+  print("Carat!! You found Hoshi!")
+elif choice1 == "7":
+  print("Carat!! You found The8!")
+elif choice1 == "8":
+  print("Carat!! You found S.Coups!")
+elif choice1 == "22":
+  print("Carat!! You found Woozi!")
+elif choice1 == "16":  
+  print("Carat!! You found Seungkwan!")
+elif choice1 == "10":
+  print("Carat!! You found Jun!")
+elif choice1 == "18":
+  print("Carat!! You found DK and Vernon!")
+elif choice1 == "11":
+  print("Carat!! You found Dino!")
